22a1ada9cc6a0940a470f14fcf401a37dfbfec01
Rotimi Best <rotimiibitoyebest@yahoo.co.uk>
2019-05-02 06:47:16 +0300

    uncommented crons

--- server.js
+++ server.js
@@ -168,14 +168,14 @@ async function BreakTimeNotifier(time) {
   }
 };
 
-// cron.schedule('15 08 * * MON,TUE,WED,THU,FRI', async () => {
-//   console.log('Sending to group notification for 11:15am');
+cron.schedule('15 08 * * MON,TUE,WED,THU,FRI', async () => {
+  console.log('Sending to group notification for 11:15am');
   
-//   await BreakTimeNotifier('11:15');
-// });
+  await BreakTimeNotifier('11:15');
+});
 
-// cron.schedule('30 12 * * MON,TUE,WED,THU,FRI', async () => {
-//   console.log('Sending to group notification for 3:30pm');
+cron.schedule('30 12 * * MON,TUE,WED,THU,FRI', async () => {
+  console.log('Sending to group notification for 3:30pm');
   
-//   await BreakTimeNotifier('3:30');
-// });
+  await BreakTimeNotifier('3:30');
+});
